ce19d7e767a7874b7ba23f0dd2c86b737906d424
bwrutter <bryanwrutter96@gmail.com>
2025-01-18 20:11:38 -0300

    chor: ajustado para deploy manual na vercel

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="cadastro-clientes-react/cadastro" element={<CadastroClientePage />} />
+        <Route path="/cadastro" element={<CadastroClientePage />} />
       </Routes>
     </Router>
   );
